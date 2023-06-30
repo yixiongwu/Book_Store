27dfc5923cdcf9db2b4447081b08b6217a6f6d3d
kindywu <yixiong.wu@cn.roblox.com>
2023-06-30 21:11:25 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ const express = require("express");
 const { Book } = require("./model/Book");
 const { BookService } = require("./service/BookService");
 
-const app = express();
-app.use(express.json());
 
 dotenv.config();
 
@@ -15,21 +13,32 @@ const book_store_name = process.env.BOOK_STORE_NAME;
 const redis_host = process.env.REDIS_HOST;
 const redis_port = process.env.REDIS_PORT;
 
+// console.log(`host=${host}, port=${port}`);
+
 // let bookService = new BookService(book_store_name);
 let bookService = new BookService(book_store_name, redis_host, redis_port);
 
-console.log(`host=${host}, port=${port}`);
 
-app.get("/", (req, res) => {
+const app = express();
+app.use(express.json());
+
+// 静态资源
+app.use('/', express.static(__dirname + '/public'));
+
+// 动态API接口
+var api = express.Router();
+app.use('/api', api);
+
+api.get("/", (req, res) => {
   res.send(`Book Store Service Online. Now is ${new Date().toLocaleString()}`);
 });
 
-app.get("/getAll", async (_, res) => {
+api.get("/getAll", async (_, res) => {
   let bookList = await bookService.getAll();
   res.json(bookList);
 });
 
-app.get("/getById/:id", async (req, res) => {
+api.get("/getById/:id", async (req, res) => {
   let id = parseInt(req.params.id);
   let book = await bookService.getById(id);
   if (book) {
@@ -39,20 +48,20 @@ app.get("/getById/:id", async (req, res) => {
   }
 });
 
-app.post("/create", async (req, res) => {
+api.post("/create", async (req, res) => {
   let book = Book.fromObject(req.body);
   let result = await bookService.create(book);
   res.json(result);
 });
 
-app.post("/update/:id", async (req, res) => {
+api.post("/update/:id", async (req, res) => {
   let id = parseInt(req.params.id);
   let book = Book.fromObject(req.body);
   let result = await bookService.update(id, book);
   res.json(result);
 });
 
-app.get("/delete/:id", async (req, res) => {
+api.get("/delete/:id", async (req, res) => {
   let id = parseInt(req.params.id);
   let result = await bookService.delete(id);
   res.json(result);
